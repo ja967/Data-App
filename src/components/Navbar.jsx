c93d3a24af3f7e74f23241a0ff7dc1370bd9d31a
ja967 <jaafarabdallah52@gmail.com>
2024-08-25 20:57:53 +0300

    Second Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,25 +11,25 @@ const Navbar = () => {
 
   return (
     <div className='text-white flex justify-between items-center h-24 max-w-[1240px] m-auto px-4'>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] cursor-pointer'>REACT.</h1>
         <ul className='hidden md:flex'>
-            <li className='p-4'>Home</li>
-            <li className='p-4'>Company</li>
-            <li className='p-4'>Resources</li>
-            <li className='p-4'>About</li>
-            <li className='p-4'>Contact</li>
+            <li className='p-4 cursor-pointer'>Home</li>
+            <li className='p-4 cursor-pointer'>Company</li>
+            <li className='p-4 cursor-pointer'>Resources</li>
+            <li className='p-4 cursor-pointer'>About</li>
+            <li className='p-4 cursor-pointer'>Contact</li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
-            {!nav ? <AiOutlineMenu size={20} /> : <AiOutlineClose size={20} />}
+            {!nav ? <AiOutlineMenu className='cursor-pointer' size={20} /> : <AiOutlineClose className='cursor-pointer' size={20} />}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] border-r border-r-gray-900 h-full bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%]'}>
             <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
             <ul className='uppercase p-4'>
-                <li className='p-4 border-b border-gray-600'>Home</li>
-                <li className='p-4 border-b border-gray-600'>Company</li>
-                <li className='p-4 border-b border-gray-600'>Resources</li>
-                <li className='p-4 border-b border-gray-600'>About</li>
-                <li className='p-4'>Contact</li>
+                <li className='p-4 border-b border-gray-600 cursor-pointer'>Home</li>
+                <li className='p-4 border-b border-gray-600 cursor-pointer'>Company</li>
+                <li className='p-4 border-b border-gray-600 cursor-pointer'>Resources</li>
+                <li className='p-4 border-b border-gray-600 cursor-pointer'>About</li>
+                <li className='p-4 cursor-pointer'>Contact</li>
             </ul>
         </div>
     </div>
